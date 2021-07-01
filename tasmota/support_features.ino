arendst/Tasmota

Feature: IRMP Support for Tasmota
Adds https://github.com/ukw100/IRMP to Tasmota.

tasmota/support_features.ino
@@ -741,7 +741,9 @@ void ResponseAppendFeatures(void)
 #if defined(USE_ENERGY_SENSOR) && defined(USE_ENERGY_DUMMY)
     feature8 |= 0x00000020;
 #endif
-//    feature8 |= 0x00000040;
+#ifdef USE_IRMP
+    feature8 |= 0x00000040;  // xdrv_45_irmp.ino
+#endif
 //    feature8 |= 0x00000080;
 
 //    feature8 |= 0x00000100;

arendst/Sonoff-Tasmota

tasmota/support_features.ino
@@ -746,7 +746,9 @@ void ResponseAppendFeatures(void)
 #if defined(USE_I2C) && defined(USE_AM2320)
     feature8 |= 0x00000040;
 #endif
-//    feature8 |= 0x00000080;
+#ifdef USE_IRMP
+    feature8 |= 0x00000080;  // xdrv_45_irmp.ino
+#endif
 
 //    feature8 |= 0x00000100;
 //    feature8 |= 0x00000200;